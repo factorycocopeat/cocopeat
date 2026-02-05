8c5c4211fc0853e096faef08e97bc84e9e1e4e17
factorycocopeat <factorycocopeat@gmail.com>
2026-02-05 09:21:48 +0530

    Infrastructure-2

--- tcf/blueprints/products.py
+++ tcf/blueprints/products.py
@@ -3,7 +3,17 @@ from flask import Blueprint, render_template, request
 bp = Blueprint("products", __name__, url_prefix="/products")
 
 @bp.route("/cocopeat/coco-peat-5kg-blocks/")
-def infrastructure():
+def cocopeat5kgblocks():
     return render_template(
         "product/coco-peat-5kg-blocks.html"
+    )
+@bp.route("/cocopeat/cocopeat-growbags/")
+def cocopeatgrowbags():
+    return render_template(
+        "product/cocopeat-growbags.html"
+    )
+@bp.route("/cocopeat/coco-chips/")
+def cocochips():
+    return render_template(
+        "product/coco-chips.html"
     )
